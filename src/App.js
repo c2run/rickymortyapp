6e92abab3c52df5cd0863c7768e1bedf86548156
c2run <jose.cerda66@gmail.com>
2023-03-22 13:56:44 -0300

    integración de api rest rick y morty

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import imagenRickyMorty from './img/rickymorty.png';
 import './App.css';
 
 function App() {
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    console.log(characterApi);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick y Morty</h1>
         <img src={imagenRickyMorty} alt="Rick y Morty" className='img-home' />
+        <button onClick={reqApi} className="btn-search">Buscar personajes</button>
       </header>
     </div>
   );
