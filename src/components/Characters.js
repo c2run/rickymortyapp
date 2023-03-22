d42faaa82bc5f7927d0d447c6eb081c3fdddc47b
c2run <jose.cerda66@gmail.com>
2023-03-22 14:41:08 -0300

    recorriendo los datos que vienen de la api

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,17 @@
 export default function Characters(props){
-
+    const {characters} = props;
+    console.log(characters);
     return (
-        <div>
-            <h1>Desde Character JS</h1>
+        <div className="characters">
+            <h1>Personajes</h1>
+            <span className="back-home">Volver al home</span>
+            <div className="containter-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                    <p>{character.name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
